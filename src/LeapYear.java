58d096dee3cc94c2b17453d03871c89779df97b4
mariellehinrichs <mariellehinrichs@localhost>
2022-09-08 13:54:46 -0500

    Done

---
+++ src/LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the year you'd like to determine whether it's a leap year: ");
+		int year = in.nextInt();
+		boolean leapYear = ((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0);	
+		System.out.println(leapYear);
+		
+	}
+
+}
